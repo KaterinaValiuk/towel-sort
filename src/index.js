896197098e46d141264e14fb9ac9e60162e9816a
KaterinaValiuk <e5054404@gmail.com>
2021-08-27 15:03:53 +0300

    task sucs has been successfully completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
-// You should implement your task here.
+/*jshint esversion: 8 */
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let result  = [];
+  let n = 0;
+  
+  if (!matrix  || matrix.length === undefined) {result  = [];} else {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let columnInd = i %2 === 0 ? j : (matrix[i].length - j - 1);
+      //console.log(result, matrix[i][columnInd]);
+      
+      result[n]= matrix[i][columnInd];
+      console.log(result, matrix[i][columnInd]);
+      n++;
+    } 
+  }}
+ return result;
+};
+
+  
